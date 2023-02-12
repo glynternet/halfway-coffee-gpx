e286fa2f3edf004987e6ec01480c49a0c75149e0
Glyn Owen Hanmer <1295698+glynternet@users.noreply.github.com>
2023-02-12 17:58:53 +0000

    🐛 escape ampersands in cafe names (#7)
    Co-authored-by: Automated <actions@users.noreply.github.com>

--- convert-data-to-gpx.sh
+++ convert-data-to-gpx.sh
@@ -5,4 +5,4 @@ cafesData="${1:?must provide cafes data file as argument to script}"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cat "$SCRIPT_DIR/header" <(jq -r '.[]| "  <wpt lat=\"\(.latitude)\" lon=\"\(.longitude)\">\n    <name>\(.title)</name>\n    <sym>Flag, Blue</sym>\n    <type>user</type>\n  </wpt>\n"' "$cafesData") <(echo "</gpx>")
+cat "$SCRIPT_DIR/header" <(jq -r '.[]| "  <wpt lat=\"\(.latitude)\" lon=\"\(.longitude)\">\n    <name>\(.title | sub("&";"&amp;"))</name>\n    <sym>Flag, Blue</sym>\n    <type>user</type>\n  </wpt>\n"' "$cafesData") <(echo "</gpx>")
